f99d0d15eac3f828473240cdaf1f68586a510483
Jorge Borges <JDBorges187@gmail.com>
2022-02-12 18:28:08 -0500

    add components, fetch top level data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReposTable from './components/ReposTable'
 import TopLevel from './components/TopLevel'
 import EventsTable from './components/EventsTable'
 import MembersTable from './components/MembersTable'
 
 export default function App() {
+
+  const [topLevel, setTopLevel] = useState([])
+  const [urls, setUrls] = useState([])
+
+  useEffect(() => {
+    console.log("Hello from App.js")
+    async function fetchData() {
+        const res = await fetch('https://api.github.com/orgs/boomtownroi');
+        const resData = await res.json();
+        setTopLevel(Object.entries(resData))
+        console.log(resData.repos_url)
+    }
+    fetchData();
+}, [])
+
   return (
     <>
-      <h1>App</h1>
-      <TopLevel />
+      <h1>Jorge Borges Tech Assessment</h1>
+      <TopLevel topLevel={topLevel} />
       <ReposTable />
       <EventsTable />
       <MembersTable />
--- src/components/TopLevel/index.js
+++ src/components/TopLevel/index.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Alert } from 'react-bootstrap';
 
-export default function TopLevel() {
-  return (
-    <div>TopLevel</div>
-  )
+
+export default function TopLevel({ topLevel }) {
+
+    const [topLevelData, setTopLevelData] = useState([])
+
+    useEffect(() => {
+        setTopLevelData(topLevel.map(([key, value]) => {
+            const variant = key.includes('url') ?
+                value.includes('api.github.com/orgs/BoomTownROI/') ? 'success' : 'danger' : 'primary'
+            return (
+                <Alert key={key} variant={variant}>
+                    <b>{key}:</b> {value}
+                </Alert>
+            );
+        }))
+    }, [topLevel])
+
+
+
+    return (
+        <>
+            <h2>Top Level Response</h2>
+            <ul>{topLevelData || null}</ul>
+        </>
+    )
 }
