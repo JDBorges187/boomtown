711f7bf1484f3a27d1913502e8bd7c8496f570a2
Jorge Borges <JDBorges187@gmail.com>
2022-02-12 21:03:19 -0500

    repo table loaded

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Container, Row , Col} from 'react-bootstrap'
 import ReposTable from './components/ReposTable'
 import TopLevel from './components/TopLevel'
 import EventsTable from './components/EventsTable'
@@ -6,27 +7,47 @@ import MembersTable from './components/MembersTable'
 
 export default function App() {
 
-  const [topLevel, setTopLevel] = useState([])
-  const [urls, setUrls] = useState([])
+  const [topLevel, setTopLevel] = useState([]);
+  const [reposUrl, setReposUrl] = useState("");
+  const [eventsUrl, setEventsUrl] = useState("");
+  const [hooksUrl, setHooksUrl] = useState("");
+  const [issuesUrl, setIssuesUrl] = useState("");
+  const [membersUrl, setMembersUrl] = useState("");
 
   useEffect(() => {
     console.log("Hello from App.js")
     async function fetchData() {
-        const res = await fetch('https://api.github.com/orgs/boomtownroi');
-        const resData = await res.json();
-        setTopLevel(Object.entries(resData))
-        console.log(resData.repos_url)
+      const res = await fetch('https://api.github.com/orgs/boomtownroi');
+      const resData = await res.json();
+      setTopLevel(Object.entries(resData))
+      setReposUrl(resData.repos_url)
+      setEventsUrl(resData.events_url)
+      setHooksUrl(resData.hooks_url)
+      setIssuesUrl(resData.issues_url)
+      setMembersUrl(resData.members_url)
     }
     fetchData();
-}, [])
+  }, [])
 
   return (
     <>
-      <h1>Jorge Borges Tech Assessment</h1>
-      <TopLevel topLevel={topLevel} />
-      <ReposTable />
-      <EventsTable />
-      <MembersTable />
+      <Container>
+        <Row>
+          <Col><h1>Jorge Borges Tech Assessment</h1></Col>
+        </Row>
+        {/* <Row>
+          <Col><TopLevel topLevel={topLevel} /></Col>
+        </Row> */}
+        <Row>
+          <Col><ReposTable reposUrl={reposUrl}/></Col>
+        </Row>
+        <Row>
+          <Col><EventsTable eventsUrl={eventsUrl}/></Col>
+        </Row>
+        <Row>
+          <Col><MembersTable membersUrl={membersUrl} /></Col>
+        </Row>
+      </Container>      
     </>
   )
 }
--- src/components/EventsTable/index.js
+++ src/components/EventsTable/index.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Table} from 'react-bootstrap';
 
-export default function EventsTable() {
+export default function EventsTable({ eventsUrl }) {
   return (
-    <div>EventsTable</div>
+    <>
+            <h2>Events Table</h2>
+            <p>{eventsUrl}</p>
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>
   )
 }
